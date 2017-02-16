fb40fb556ac2f30b47cf2a129e47e319200b7fdd
Dan Nixon <dan@dan-nixon.com>
2017-02-16 12:46:45 +0000

    fix

--- scripts/pyspark-notebook.sh
+++ scripts/pyspark-notebook.sh
@@ -3,6 +3,6 @@
 # This script will launch pyspark in an ipython notebook
 
 export PYSPARK_DRIVER_PYTHON=ipython
-export PYSPARK_DRIVER_PYTHON_OPTS='notebook' pyspark
+export PYSPARK_DRIVER_PYTHON_OPTS='notebook --ip="*"' pyspark
 
 pyspark
