9e753301ff02338b6530f1fbcbc81aae0b89eef3
Dan Nixon <dan@dan-nixon.com>
2017-02-20 11:50:25 +0000

    CW3 wip

--- cw3_spark_streaming/cw3_spark.py
+++ cw3_spark_streaming/cw3_spark.py
@@ -1,4 +1,5 @@
-import logging
+import datetime
+import json
 from pyspark import SparkContext, SparkConf, RDD
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
@@ -54,7 +55,7 @@ def send_to_cassandra(rdd, table_name):
     (rdd_df.write.format("org.apache.spark.sql.cassandra").mode('append').options(table=table_name, keyspace="csc8101").save())
 
 
-########## Spark Streaming Setup ##########
+# Spark Streaming Setup
 
 # Create a conf object to hold connection information
 sp_conf = SparkConf()
@@ -68,24 +69,23 @@ sp_conf.set("spark.cassandra.connection.host", "localhost")
 # For local development it is very important to use "local[2]" as Spark
 # Streaming needs 2 cores minimum to function properly
 sc = SparkContext(master="local[2]", appName="Event Processing", conf=sp_conf)
+sc.setLogLevel("OFF")
 
-#Set the batch interval in seconds
+# Set the batch interval in seconds
 BATCH_INTERVAL = 10
 
-#Create the streaming context object
+# Create the streaming context object
 ssc = StreamingContext(sc, BATCH_INTERVAL)
 
 
-########## Kafka Setup ##########
+#
+# TASK 1
+# Kafka setup
+#
 
 # Create receiver based stream
-# NOTE: Please edit the Broker IP to match your designated broker address.
-# Also edit the client_id_for_broker to be something personal to you such as
-# your student number. This is used for the broker to identify your client.
-# Also edit the topic_name you would like to pull messages from and the number
-# of partitions to consume on that topic. For development 1 is fine, for
-# consuming from the production stream a higher number is recommended.
 topic_name = "dev-stream"
+# topic_name = "production"
 client_id_for_broker = "120263697"
 num_of_partition_to_consume_from = 1
 raw_messages = KafkaUtils.createStream(ssc,
@@ -93,18 +93,55 @@ raw_messages = KafkaUtils.createStream(ssc,
         client_id_for_broker,
         {topic_name: num_of_partition_to_consume_from})
 
-########## Window the incoming batches ##########
 
-########## Convert each message from json into a dictionary ##########
+#
+# TASK 2
+# Window the incoming batches
+#
 
-########## Find the minimum timestamp for window ##########
+# window = raw_messages.window(120, 60)
+window = raw_messages.window(20, 10)
+
+# window.pprint(10)
 
 
-############################## Task 1 ##############################
+#
+# TASK 3
+# Convert each message from json into a dictionary
+#
 
-#### Calculate the page visits for each client id ####
+def json_to_event(data):
+    json_data = json.loads(data[1])
+    time = datetime.datetime.strptime(
+        json_data["timestamp"], '%Y-%m-%dT%H:%M:%S')
+    ts = int((time - datetime.datetime(1970, 1, 1)).total_seconds() * 1e3)
+    return (json_data["client_id"], ts, json_data["url"]["topic"], json_data["url"]["page"])
 
-#### Write the Task 1 results to cassandra ####
+events = window.map(json_to_event)
+
+events.pprint(10)
+
+
+#
+# TASK 4
+# Find the minimum timestamp for window
+#
+
+def find_lowest_timestamp(rdd):
+    ts = rdd.aggregate(float('inf'), lambda a, x: min(a, x[1]), lambda a, b: min(a, b))
+    return sc.parallelize([ts])
+
+timestamps = events.transform(find_lowest_timestamp)
+
+timestamps.pprint(10)
+
+
+#
+# TASK 5
+# Calculate the page visits for each client ID
+#
+
+# TODO
 
 # Convert each element of the rdd_example rdd into a Row
 # NOTE: This is just example code please rename the DStreams and Row fields to
@@ -118,14 +155,14 @@ raw_messages = KafkaUtils.createStream(ssc,
 # Convert each rdd of Rows in the DStream into a DataFrame and send to Cassandra
 # row_rdd_example.foreachRDD(lambda rdd: send_to_cassandra(rdd, your_table_name))
 
+#
+# TASK 6
+# Calculate the unique views per page
+#
 
-############################## Task 2 ##############################
+# TODO
 
-#### Calculate the unique views per page
-
-#### Write the Task 2 results to cassandra ####
 
 # Initiate the stream processing
 ssc.start()
 ssc.awaitTermination()
-
